dd669c6af7c134a909211feb9dc34e2c1fc4676e
alex <a.kom90@mail.ru>
2017-07-15 14:26:05 +0300

    Create base structure.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 var $ = require('jquery');
 var str = require('./constant');
 
-var App = function () {
-	console.log(str);
-	$('body').html(str);
+var App = function() {
+    // console.log(str);
+    $('body').html(str);
 };
 
 var app = new App();
+app.get = function() {};
